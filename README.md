c31cb5d9d689039b51c68f8adc43679299ba443f
awss1236 <awsssayah@gmail.com>
2025-07-18 00:03:23 +0100

    Finally made the utility a utility.

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-## Gamrer
+# Gamrer
 This is a utility that allows for transforming Backus Naur form
 grammars into directly usable parsers in haskell.
+
+## Usage
+Right now I don't ship an executable so you'll have to compile it,
+which is not difficult as the program doesn't have any dependencies
+outside of the standard libraries yet. (Consult `build.sh`)
+
+After you've built it you can use it directly by giving it the path
+of a file containing the grammar:
+```
+./main FILE
+```
+The Program outputs to stdout though so you may want to do a little
+bit of shell magic:
+```
+./main FILE >> OUT.hs
+```
+Where `OUT.hs` would then contain the output code.
+
+## Using the generated code
+Making the generated code more usable is a big TODO for now.
+But you can still copy paste the genertated code into an app
+although I really wouldn't reccomned it (It's really ugly
+right now).
+You may also wrap it into a module.
--- main.hs
+++ main.hs
@@ -182,26 +182,33 @@ genRuleCode (Rule name cs) =
           (f : r') = groupS s
        in ((bef, f), r')
 
+printUsage :: IO ()
+printUsage = putStrLn "USAGE: ./main FILE\nwhere the FILE is the file containing the grammar.\nThe program outputs to stdout."
+
 main :: IO ()
 main =
   do
     args <- getArgs
-    inp <-
-      NE.fromList
-        <$> filter (/= "")
-        <$> lines
-        <$> readFile "an-exp.txt"
+    if null args
+      then
+        printUsage
+      else do
+        inp <-
+          NE.fromList
+            <$> filter (/= "")
+            <$> lines
+            <$> readFile (NE.head $ NE.fromList args)
 
-    let [_, toks'] = splitOn ":=" (NE.head inp)
+        let [_, toks'] = splitOn ":=" (NE.head inp)
 
-    let toks = map trim $ splitOn "," toks'
+        let toks = map trim $ splitOn "," toks'
 
-    let rules' = NE.tail inp
+        let rules' = NE.tail inp
 
-    let rules = parseRules rules'
+        let rules = parseRules rules'
 
-    putStrLn $ "import Control.Applicative\n\ndata Token = " ++ intercalate " | " (map (\t -> "T" ++ t) toks) ++ " deriving (Show, Eq)"
-    putStrLn $ intercalate "\n" (map genRuleType rules)
-    putStrLn $ "newtype Parser a = Parser {runParser :: [Token] -> Maybe (a, [Token])}\n\ninstance Functor Parser where\n  fmap f (Parser a) = Parser $ \\s -> do\n    (x, input') <- a s\n    Just (f x, input')\n\ninstance Applicative Parser where\n  pure a = Parser $ \\t -> Just (a, t)\n  (<*>) (Parser f) (Parser a) = Parser $ \\s -> do\n    (jf, in1) <- f s\n    (ja, in2) <- a in1\n    Just (jf ja, in2)\n\ninstance Alternative Parser where\n  empty = Parser $ const Nothing\n  (Parser a) <|> (Parser b) = Parser $ \\s -> a s <|> b s\n\ntokenParser :: Token -> Parser Token\ntokenParser t = Parser $ \\ts -> case ts of\n  (t1 : rest) -> if t == t1 then Just (t, rest) else Nothing\n  _ -> Nothing\n"
+        putStrLn $ "import Control.Applicative\n\ndata Token = " ++ intercalate " | " (map (\t -> "T" ++ t) toks) ++ " deriving (Show, Eq)"
+        putStrLn $ intercalate "\n" (map genRuleType rules)
+        putStrLn $ "newtype Parser a = Parser {runParser :: [Token] -> Maybe (a, [Token])}\n\ninstance Functor Parser where\n  fmap f (Parser a) = Parser $ \\s -> do\n    (x, input') <- a s\n    Just (f x, input')\n\ninstance Applicative Parser where\n  pure a = Parser $ \\t -> Just (a, t)\n  (<*>) (Parser f) (Parser a) = Parser $ \\s -> do\n    (jf, in1) <- f s\n    (ja, in2) <- a in1\n    Just (jf ja, in2)\n\ninstance Alternative Parser where\n  empty = Parser $ const Nothing\n  (Parser a) <|> (Parser b) = Parser $ \\s -> a s <|> b s\n\ntokenParser :: Token -> Parser Token\ntokenParser t = Parser $ \\ts -> case ts of\n  (t1 : rest) -> if t == t1 then Just (t, rest) else Nothing\n  _ -> Nothing\n"
 
-    putStrLn $ intercalate "\n\n" (map genRuleCode rules)
+        putStrLn $ intercalate "\n\n" (map genRuleCode rules)
