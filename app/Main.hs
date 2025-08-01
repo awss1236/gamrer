6edd7a083efb783ce5296865ea3a755965887141
awss1236 <awsssayah@gmail.com>
2025-08-01 14:54:14 +0100

    Cleaned up main

--- app/Main.hs
+++ app/Main.hs
@@ -18,26 +18,23 @@ main :: IO ()
 main =
   do
     args <- getArgs
-    if null args
-      then
-        printUsage
-      else do
-        inp <-
-          NE.fromList
-            <$> filter (/= "")
-            <$> lines
-            <$> readFile (NE.head $ NE.fromList args)
+    when (null args) printUsage
+    inp <-
+      NE.fromList
+        <$> filter (/= "")
+        <$> lines
+        <$> readFile (NE.head $ NE.fromList args)
 
-        let [_, toks'] = splitOn ":=" (NE.head inp)
+    let [_, toks'] = splitOn ":=" (NE.head inp)
 
-        let toks = map trim $ splitOn "," toks'
+    let toks = map trim $ splitOn "," toks'
 
-        let rules' = NE.tail inp
+    let rules' = NE.tail inp
 
-        let rules = readRules rules'
+    let rules = readRules rules'
 
-        putStrLn $ "import Control.Applicative\n\ndata Token = " ++ intercalate " | " (map (\t -> "T" ++ t) toks) ++ " deriving (Show, Eq)"
-        putStrLn $ intercalate "\n" (map genRuleType rules)
-        putStrLn $ "newtype Parser a = Parser {runParser :: [Token] -> Maybe (a, [Token])}\n\ninstance Functor Parser where\n  fmap f (Parser a) = Parser $ \\s -> do\n    (x, input') <- a s\n    Just (f x, input')\n\ninstance Applicative Parser where\n  pure a = Parser $ \\t -> Just (a, t)\n  (<*>) (Parser f) (Parser a) = Parser $ \\s -> do\n    (jf, in1) <- f s\n    (ja, in2) <- a in1\n    Just (jf ja, in2)\n\ninstance Alternative Parser where\n  empty = Parser $ const Nothing\n  (Parser a) <|> (Parser b) = Parser $ \\s -> a s <|> b s\n\ntokenParser :: Token -> Parser Token\ntokenParser t = Parser $ \\ts -> case ts of\n  (t1 : rest) -> if t == t1 then Just (t, rest) else Nothing\n  _ -> Nothing\n"
+    putStrLn $ "import Control.Applicative\n\ndata Token = " ++ intercalate " | " (map (\t -> "T" ++ t) toks) ++ " deriving (Show, Eq)"
+    putStrLn $ intercalate "\n" (map genRuleType rules)
+    putStrLn $ "newtype Parser a = Parser {runParser :: [Token] -> Maybe (a, [Token])}\n\ninstance Functor Parser where\n  fmap f (Parser a) = Parser $ \\s -> do\n    (x, input') <- a s\n    Just (f x, input')\n\ninstance Applicative Parser where\n  pure a = Parser $ \\t -> Just (a, t)\n  (<*>) (Parser f) (Parser a) = Parser $ \\s -> do\n    (jf, in1) <- f s\n    (ja, in2) <- a in1\n    Just (jf ja, in2)\n\ninstance Alternative Parser where\n  empty = Parser $ const Nothing\n  (Parser a) <|> (Parser b) = Parser $ \\s -> a s <|> b s\n\ntokenParser :: Token -> Parser Token\ntokenParser t = Parser $ \\ts -> case ts of\n  (t1 : rest) -> if t == t1 then Just (t, rest) else Nothing\n  _ -> Nothing\n"
 
-        putStrLn $ intercalate "\n\n" (map genRuleCode rules)
+    putStrLn $ intercalate "\n\n" (map genRuleCode rules)
