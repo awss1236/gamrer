a0e5336a1797a018a8d251a7bad9f641a3373aa7
awss1236 <awsssayah@gmail.com>
2025-08-09 01:45:57 +0100

    Finished implementing preparser.

---
+++ app/BNFPreParser.hs
@@ -0,0 +1,31 @@
+module PreParser (tokensToPreRules, PreRule (..)) where
+
+import BNFLexer (Token (..), lexTokens)
+import Data.List.Split
+import qualified Data.Map as M
+
+data PreRule = PreRule String [[Token]] deriving (Show)
+
+addPreRule :: PreRule -> M.Map String [[Token]] -> M.Map String [[Token]]
+addPreRule (PreRule n tss) rs = M.insertWith (++) n tss rs
+
+findDefs :: [Token] -> [[Token]]
+findDefs ts = drop 1 $ findDefs' ts []
+  where
+    findDefs' :: [Token] -> [Token] -> [[Token]]
+    findDefs' (n : Definition : r) acc = acc : (findDefs' r [n, Definition])
+    findDefs' (n : DefAlternate : r) acc = acc : (findDefs' r [n, DefAlternate])
+    findDefs' (t : r) acc = findDefs' r (acc ++ [t])
+    findDefs' [] acc = [acc]
+
+defToPreRule :: [Token] -> PreRule
+defToPreRule (NonTerminal n : _ : r) = PreRule n (splitOn [Alternate] r)
+defToPreRule _ = error "unreachable defToPreRule from module BNFPreParser"
+
+tokensToPreRules :: [Token] -> [PreRule]
+tokensToPreRules ts =
+  let defs = findDefs ts
+      prs = map defToPreRule defs
+   in map (uncurry PreRule) $
+        M.toList $
+          foldr addPreRule M.empty prs
